c238d696ac407922ffde51bf7ab1c5eab62da94b
CameronRuch <cameron.ruch@yahoo.com>
2022-04-18 23:15:38 -0500

    formatted

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,34 +1,34 @@
 // add middlewares here related to actions
 const Action = require('./actions-model')
 
-async function validateActionId(req,res,next) {
-    try{
-    const action = await Action.get(req.params.id)
-    if(action){
-        req.action = action
-        next()
-    }else {
-        next({
-            status: 404,
-            message: "No project found"
-        })
-    }
-    }catch(err){
+async function validateActionId(req, res, next) {
+    try {
+        const action = await Action.get(req.params.id)
+        if (action) {
+            req.action = action
+            next()
+        } else {
+            next({
+                status: 404,
+                message: "No project found"
+            })
+        }
+    } catch (err) {
         next
     }
 }
-async function validateAction(req,res,next) {
-    try{
-        const {description, notes } = req.body
-        if (!notes || !description ){
+async function validateAction(req, res, next) {
+    try {
+        const { description, notes } = req.body
+        if (!notes || !description) {
             res.status(400).json({
                 message: "Missing valid notes or description"
             })
-        }else{
+        } else {
             next()
         }
     }
-    catch(err){
+    catch (err) {
         console.log(err)
     }
 }
